61883cbf493effcb942dbecac9ad430a8b5d6ca9
juzun <zaplejtalja@gmail.com>
2023-11-09 18:31:05 +0100

    add openpyxl

--- strlt.py
+++ strlt.py
@@ -12,6 +12,16 @@ if uploaded_file is not None:
 else:
   st.info('Nics nenačet.')
 
+# uploaded_file = st.file_uploader("Choose a file")
+
+# if uploaded_file is not None:
+#   df = pd.read_csv(uploaded_file)
+#   st.subheader('DataFrame')
+#   st.write(df)
+#   st.subheader('Descriptive Statistics')
+#   st.write(df.describe())
+# else:
+#   st.info('☝️ Upload a CSV file')
 
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
