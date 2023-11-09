2d8ff5d49574a445be8d049ada1021e6f5120182
juzun <zaplejtalja@gmail.com>
2023-11-09 13:58:21 +0100

    qqqqqq

--- strlt.py
+++ strlt.py
@@ -1,15 +1,31 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as po
 
-st.header('Line chart')
+st.header('st.selectbox')
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+option = st.selectbox(
+     'What is your favorite color?',
+     ('Blue', 'Red', 'Green'))
 
-fig = po.Figure()
-fig.add_trace(po.Scatter(x=chart_data.index, y=chart_data['a']))
+st.write('Your favorite color is ', option)
 
-st.plotly_chart(fig, use_container_width=True)
+
+# import plotly.graph_objects as po
+
+# st.header('Line chart')
+
+# chart_data = pd.DataFrame(
+#      np.random.randn(20, 3),
+#      columns=['a', 'b', 'c'])
+
+# fig = po.Figure()
+# fig.add_trace(po.Scatter(x=chart_data.index, y=chart_data['a']))
+
+# st.plotly_chart(fig, use_container_width=True)
+
+# import plotly.express as px
+
+# fig2 = px.line(chart_data)
+
+# st.plotly_chart(fig2)
