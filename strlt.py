461e84f5443a26b38bf90364d98ce1c4ed9f86c9
juzun <zaplejtalja@gmail.com>
2023-11-09 17:04:08 +0100

    add poetry

--- strlt.py
+++ strlt.py
@@ -2,40 +2,21 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+import plotly.graph_objects as po
 
-st.header('st.checkbox')
+st.header('Line chart')
 
-st.write ('What would you like to order?')
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
 
-icecream = st.checkbox('Ice cream')
-coffee = st.checkbox('Coffee')
-cola = st.checkbox('Cola')
+fig = po.Figure()
+fig.add_trace(po.Scatter(x=chart_data.index, y=chart_data['a']))
 
-if icecream:
-     st.write("Great! Here's some more 🍦")
+st.plotly_chart(fig, use_container_width=True)
 
-if coffee: 
-     st.write("Okay, here's some coffee ☕")
+import plotly.express as px
 
-if cola:
-     st.write("Here you go 🥤")
+fig2 = px.line(chart_data)
 
-
-# import plotly.graph_objects as po
-
-# st.header('Line chart')
-
-# chart_data = pd.DataFrame(
-#      np.random.randn(20, 3),
-#      columns=['a', 'b', 'c'])
-
-# fig = po.Figure()
-# fig.add_trace(po.Scatter(x=chart_data.index, y=chart_data['a']))
-
-# st.plotly_chart(fig, use_container_width=True)
-
-# import plotly.express as px
-
-# fig2 = px.line(chart_data)
-
-# st.plotly_chart(fig2)
+st.plotly_chart(fig2)
