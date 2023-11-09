a56c2cb4d45238aa64a215580cdd25102bf6eecd
juzun <zaplejtalja@gmail.com>
2023-11-09 13:59:53 +0100

    rrrrr

--- strlt.py
+++ strlt.py
@@ -2,14 +2,23 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.header('st.multiselect')
 
-options = st.multiselect(
-     'What are your favorite colors',
-     ['Green', 'Yellow', 'Red', 'Blue'],
-     ['Yellow', 'Red'])
+st.header('st.checkbox')
 
-st.write('You selected:', options)
+st.write ('What would you like to order?')
+
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+if icecream:
+     st.write("Great! Here's some more 🍦")
+
+if coffee: 
+     st.write("Okay, here's some coffee ☕")
+
+if cola:
+     st.write("Here you go 🥤")
 
 
 # import plotly.graph_objects as po
