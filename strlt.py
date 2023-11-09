45eeabbdef6af53d2b0425a91aa1f8cbbe69c627
juzun <zaplejtalja@gmail.com>
2023-11-09 17:31:57 +0100

    latex

--- strlt.py
+++ strlt.py
@@ -1,21 +1,18 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
 
-import plotly.graph_objects as po
 
-st.header('Line chart')
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
 
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
-
-fig = po.Figure()
-fig.add_trace(po.Scatter(x=chart_data.index, y=chart_data['a']))
-
-st.plotly_chart(fig, use_container_width=True)
-
-import plotly.express as px
 
 fig2 = px.line(chart_data)
 
