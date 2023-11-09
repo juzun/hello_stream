9c0526eca79dd0af9e9d30a1693a360b88c29b0f
juzun <zaplejtalja@gmail.com>
2023-11-09 13:40:24 +0100

    aaaaaa

--- strlt.py
+++ strlt.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.graph_objects as po
 
 st.header('Line chart')
 
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
+
+fig = po.Figure()
+fig.add_trace(po.Scatter(chart_data))
 
 st.plotly_chart(chart_data)
