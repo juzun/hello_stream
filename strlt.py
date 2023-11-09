e8c4402fd9f63ad5a46c5983dc07835475bd92dd
juzun <zaplejtalja@gmail.com>
2023-11-09 13:08:39 +0100

    aa

--- strlt.py
+++ strlt.py
@@ -3,36 +3,8 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-st.header('st.write')
 
-# Example 1
+st.header('Jezdítko')
 
-st.subheader('Display text')
-st.write('Hello, *World!* :sunglasses:')
-
-# Example 2
-
-st.write(1234)
-
-# Example 3
-
-df = pd.DataFrame({
-     'first column': [1, 2, 3, 4],
-     'second column': [10, 20, 30, 40]
-     })
-st.write(df)
-
-# Example 4
-
-st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
-
-# Example 5
-
-df2 = pd.DataFrame(
-     np.random.randn(200, 3),
-     columns=['a', 'b', 'c'])
-c = alt.Chart(df2).mark_circle().encode(
-     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-st.write(c)
-
-st.write(df2)
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')
