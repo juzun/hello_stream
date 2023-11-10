84f77af21c03a1926232540c34369d7a081de5ad
juzun <zaplejtalja@gmail.com>
2023-11-10 11:59:00 +0100

    form2

--- strlt.py
+++ strlt.py
@@ -37,16 +37,16 @@ with col2:
     with st.form('my_form'):
         st.subheader('**Order your coffee**')
 
-    # Input widgets
-    coffee_bean_val = st.selectbox('Coffee bean', ['Arabica', 'Robusta'])
-    coffee_roast_val = st.selectbox('Coffee roast', ['Light', 'Medium', 'Dark'])
-    brewing_val = st.selectbox('Brewing method', ['Aeropress', 'Drip', 'French press', 'Moka pot', 'Siphon'])
-    serving_type_val = st.selectbox('Serving format', ['Hot', 'Iced', 'Frappe'])
-    milk_val = st.select_slider('Milk intensity', ['None', 'Low', 'Medium', 'High'])
-    owncup_val = st.checkbox('Bring own cup')
+        # Input widgets
+        coffee_bean_val = st.selectbox('Coffee bean', ['Arabica', 'Robusta'])
+        coffee_roast_val = st.selectbox('Coffee roast', ['Light', 'Medium', 'Dark'])
+        brewing_val = st.selectbox('Brewing method', ['Aeropress', 'Drip', 'French press', 'Moka pot', 'Siphon'])
+        serving_type_val = st.selectbox('Serving format', ['Hot', 'Iced', 'Frappe'])
+        milk_val = st.select_slider('Milk intensity', ['None', 'Low', 'Medium', 'High'])
+        owncup_val = st.checkbox('Bring own cup')
 
-    # Every form must have a submit button
-    submitted = st.form_submit_button('Submit')
+        # Every form must have a submit button
+        submitted = st.form_submit_button('Submit')
 
     if submitted:
         st.markdown(f'''
