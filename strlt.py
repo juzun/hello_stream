20df059f2687862fe2cc832209a4ccbf880f16fe
juzun <zaplejtalja@gmail.com>
2023-11-10 12:07:11 +0100

    query

--- strlt.py
+++ strlt.py
@@ -5,84 +5,105 @@ import plotly.express as px
 import time
 
 
-st.set_page_config(layout="wide")
-
-st.title('How to layout your Streamlit app')
-
 with st.expander('About this app'):
-    st.write('This app shows the various ways on how you can layout your Streamlit app.')
-    st.image('https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png', width=250)
+    st.write("`st.experimental_get_query_params` allows the retrieval of query parameters directly from the URL of the user's browser.")
 
-st.sidebar.header('Input')
-user_name = st.sidebar.text_input('What is your name?')
-user_emoji = st.sidebar.selectbox('Choose an emoji', ['', '😄', '😆', '😊', '😍', '😴', '😕', '😱'])
-user_food = st.sidebar.selectbox('What is your favorite food?', ['', 'Tom Yum Kung', 'Burrito', 'Lasagna', 'Hamburger', 'Pizza'])
+# 1. Instructions
+st.header('1. Instructions')
+st.markdown('''
+In the above URL bar of your internet browser, append the following:
+`?firstname=Jack&surname=Beanstalk`
+after the base URL `http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/`
+such that it becomes 
+`http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/?firstname=Jack&surname=Beanstalk`
+''')
 
-st.header('Output')
 
-col1, col2, col3 = st.columns(3)
+# 2. Contents of st.experimental_get_query_params
+st.header('2. Contents of st.experimental_get_query_params')
+st.write(st.experimental_get_query_params())
 
-with col1:
-    if user_name != '':
-        st.write(f'👋 Hello {user_name}!')
-    else:
-        st.write('👈  Please enter your **name**!')
 
-with col2:
-    if user_emoji != '':
-        st.write(f'{user_emoji} is your favorite **emoji**!')
-    else:
-        st.write('👈 Please choose an **emoji**!')
+# 3. Retrieving and displaying information from the URL
+st.header('3. Retrieving and displaying information from the URL')
+
+firstname = st.experimental_get_query_params()['firstname'][0]
+surname = st.experimental_get_query_params()['surname'][0]
+
+st.write(f'Hello **{firstname} {surname}**, how are you?')
+
+
+# st.set_page_config(layout="wide")
+
+# st.title('How to layout your Streamlit app')
+
+# with st.expander('About this app'):
+#     st.write('This app shows the various ways on how you can layout your Streamlit app.')
+#     st.image('https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png', width=250)
+
+# st.sidebar.header('Input')
+# user_name = st.sidebar.text_input('What is your name?')
+# user_emoji = st.sidebar.selectbox('Choose an emoji', ['', '😄', '😆', '😊', '😍', '😴', '😕', '😱'])
+# user_food = st.sidebar.selectbox('What is your favorite food?', ['', 'Tom Yum Kung', 'Burrito', 'Lasagna', 'Hamburger', 'Pizza'])
+
+# st.header('Output')
+
+# col1, col2 = st.columns(2)
+
+# with col1:
+#     if user_emoji != '':
+#         st.write(f'{user_emoji} is your favorite **emoji**!')
+#     else:
+#         st.write('👈 Please choose an **emoji**!')
   
-    with st.form('my_form'):
-        st.subheader('**Order your coffee**')
+#     with st.form('my_form'):
+#         st.subheader('**Order your coffee**')
 
-        # Input widgets
-        coffee_bean_val = st.selectbox('Coffee bean', ['Arabica', 'Robusta'])
-        coffee_roast_val = st.selectbox('Coffee roast', ['Light', 'Medium', 'Dark'])
-        brewing_val = st.selectbox('Brewing method', ['Aeropress', 'Drip', 'French press', 'Moka pot', 'Siphon'])
-        serving_type_val = st.selectbox('Serving format', ['Hot', 'Iced', 'Frappe'])
-        milk_val = st.select_slider('Milk intensity', ['None', 'Low', 'Medium', 'High'])
-        owncup_val = st.checkbox('Bring own cup')
+#         # Input widgets
+#         coffee_bean_val = st.selectbox('Coffee bean', ['Arabica', 'Robusta'])
+#         coffee_roast_val = st.selectbox('Coffee roast', ['Light', 'Medium', 'Dark'])
+#         brewing_val = st.selectbox('Brewing method', ['Aeropress', 'Drip', 'French press', 'Moka pot', 'Siphon'])
+#         serving_type_val = st.selectbox('Serving format', ['Hot', 'Iced', 'Frappe'])
+#         milk_val = st.select_slider('Milk intensity', ['None', 'Low', 'Medium', 'High'])
+#         owncup_val = st.checkbox('Bring own cup')
 
-        # Every form must have a submit button
-        submitted = st.form_submit_button('Submit')
+#         # Every form must have a submit button
+#         submitted = st.form_submit_button('Submit')
 
-    if submitted:
-        st.markdown(f'''
-            ☕ You have ordered:
-            - Coffee bean: `{coffee_bean_val}`
-            - Coffee roast: `{coffee_roast_val}`
-            - Brewing: `{brewing_val}`
-            - Serving type: `{serving_type_val}`
-            - Milk: `{milk_val}`
-            - Bring own cup: `{owncup_val}`
-            ''')
-    else:
-        st.write('☝️ Place your order!')
-
-
-    # Short example of using an object notation
-    st.header('2. Example of object notation')
-
-    form = st.form('my_form_2')
-    selected_val = form.slider('Select a value')
-    form.form_submit_button('Submit')
+#     if submitted:
+#         st.markdown(f'''
+#             ☕ You have ordered:
+#             - Coffee bean: `{coffee_bean_val}`
+#             - Coffee roast: `{coffee_roast_val}`
+#             - Brewing: `{brewing_val}`
+#             - Serving type: `{serving_type_val}`
+#             - Milk: `{milk_val}`
+#             - Bring own cup: `{owncup_val}`
+#             ''')
+#     else:
+#         st.write('☝️ Place your order!')
 
 
-with col3:
-    if user_food != '':
-        st.write(f'🍴 **{user_food}** is your favorite **food**!')
-    else:
-        st.write('👈 Please choose your favorite **food**!')
+#     # Short example of using an object notation
+#     st.header('2. Example of object notation')
+
+#     form = st.form('my_form_2')
+#     selected_val = form.slider('Select a value')
+#     form.form_submit_button('Submit')
+
+
+# with col2:
+#     if user_food != '':
+#         st.write(f'🍴 **{user_food}** is your favorite **food**!')
+#     else:
+#         st.write('👈 Please choose your favorite **food**!')
   
-    my_bar = st.progress(0)
-    for percent_complete in range(100):
-        time.sleep(0.05)
-        my_bar.progress(percent_complete + 1)
+#     my_bar = st.progress(0)
+#     for percent_complete in range(100):
+#         time.sleep(0.05)
+#         my_bar.progress(percent_complete + 1)
 
-    st.balloons()
-
+#     st.balloons()
 
 
 
